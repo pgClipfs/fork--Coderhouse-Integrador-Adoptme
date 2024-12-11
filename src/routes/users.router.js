4507f111787108b76dbdb9831232468c416f1617
Lautaro Garcia <lautarojgarcia177@gmail.com>
2024-12-11 12:48:15 -0300

    se quita la funcionalidad de subida de archivos

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,15 +1,11 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller.js';
-import uploader from '../utils/uploader.js';
 
 const router = Router();
 
 router.get('/', usersController.getAllUsers);
-
 router.get('/:uid', usersController.getUser);
 router.put('/:uid', usersController.updateUser);
 router.delete('/:uid', usersController.deleteUser);
-router.post('/:uid/documents', uploader.array('documents'), usersController.uploadDocuments);
-
 
 export default router;
